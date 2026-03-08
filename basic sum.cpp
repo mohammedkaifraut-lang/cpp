619ecf779b484f5051d2def65f5ec60f16b11896
Mohammed Kaif Raut <mohammedkaifraut@gmail.com>
2026-03-08 12:15:50 +0530

    Add basic sum program in C++

---
+++ basic sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,z,sum=0;
+    cout << "enter x:";
+    cin>>x;
+    cout << "enter y:";
+    cin>>y;
+    cout << "enter z:";
+    cin>>z;
+    sum=x+y+z;
+    cout<<"enter sum:"<<sum;
+    return 0;
+}
